e35aedf8c1d03e41c80519f264bc7b4d712f9bd1
ReinT0 <tj.rein@live.no>
2025-10-23 20:36:08 +1000

    Added DiceLoss to modules.py

--- recognition/Improved_UNET_Trond_Rein/modules.py
+++ recognition/Improved_UNET_Trond_Rein/modules.py
@@ -8,18 +8,21 @@ import torch.nn as nn
 import torch.nn.functional as F
 from utils import NUM_CLASSES
 
+
+# Helper for the UNet
 class PreActivationBlock(nn.Module):
     def __init__(self, in_ch, out_ch, dropout=0.1):
         super().__init__()
 
-        self.activation_layer = nn.SiLU()
-
         # First convolution
-        self.nrom1 = nn.InstanceNorm2d(in_ch)
-        self.conv1 = nn.Conv2d(in_ch, out_ch, kernel_size=3, padding=1),
-        self.norm2 = nn.InstanceNorm2d(out_ch),
+        self.norm1 = nn.InstanceNorm2d(in_ch)
+        self.conv1 = nn.Conv2d(in_ch, out_ch, kernel_size=3, padding=1)
+        self.norm2 = nn.InstanceNorm2d(out_ch)
         self.conv2 = nn.Conv2d(out_ch, out_ch, kernel_size=3, padding=1)
-        self.act = self.activation_layer(negative_slope=0.2, inplace=True),
+        
+        self.act = nn.LeakyReLU(negative_slope=0.2, inplace=True)
+        #self.act = nn.SiLU()
+        
         self.drop = nn.Dropout2d(dropout)
         self.skip = nn.Conv2d(in_ch, out_ch, kernel_size=1) if in_ch != out_ch else nn.Identity()
                 
@@ -54,7 +57,7 @@ class ImprovedUNet(nn.Module):
         self.decode3 = PreActivationBlock(f2+f2, f2, dropout)
 
         self.up2 = nn.ConvTranspose2d(f2, f1, kernel_size=2, stride=2)        
-        self.decode3 = PreActivationBlock(f1+f1, f1, dropout)
+        self.decode2 = PreActivationBlock(f1+f1, f1, dropout)
 
         self.up1 = nn.ConvTranspose2d(f1, f0, kernel_size=2, stride=2)        
         self.decode1 = PreActivationBlock(f0+f0, f0, dropout)
@@ -68,21 +71,42 @@ class ImprovedUNet(nn.Module):
     def forward(self, x):
         H, W = x.shape[-2:]
 
-        e1 = self.enoce1(x)
+        e1 = self.encode1(x)
         e2 = self.encode2(self.down1(e1))
-        e3 = self.encode3(self.down2(encode2))
+        e3 = self.encode3(self.down2(e2))
 
         b = self.bottleneck(self.down3(e3))
 
-        d3 = self.decode3(torch.cat([self.up3(b), e3]))
-        d2 = self.decode2(torch.cat([self.up2(d3), e2]))
-        d1 = self.decode1(torch.cat([self.up1(d2), e1]))
+        d3 = self.decode3(torch.cat([self.up3(b), e3], dim=1))
+        d2 = self.decode2(torch.cat([self.up2(d3), e2], dim=1))
+        d1 = self.decode1(torch.cat([self.up1(d2), e1], dim=1))
 
         out1 = self.segment1(d1)
-        out2 = F.interpolate(self.segment2(d2), size=(H, W), mode='bilinear')
-        out3 = F.interpolate(self.segment3(d3), size=(H, W), mode='bilinear')
+        out2 = F.interpolate(self.segment2(d2), size=(H, W), mode='bilinear', align_corners=False)
+        out3 = F.interpolate(self.segment3(d3), size=(H, W), mode='bilinear', align_corners=False)
         
         out = out1 + out2 + out3
         #out = self.softmax(out)
         
-        return out    
+        return out
+
+
+class DiceLoss(nn.Module):
+    def __init__(self, num_classes=NUM_CLASSES, smooth=1e-6):
+        super().__init__()
+        self.num_classes = num_classes
+        self.smooth = smooth
+
+    def forward(self, logits, targets):
+        probs = F.softmax(logits, dim=1)
+        target_oh = F.one_hot(target.long(), self.num_classes)
+        target_oh = target_oh.permute(0, 3, 1, 2).float()
+
+        dims = (0, 2, 3)
+
+        intersection = (probs * target_oh).sum(dims)
+        denom = (probs + target_oh).sum(dims)
+
+        dice_coeff = (2.0 * intersection + self.smooth) / (denom + self.smooth)
+
+        return 1 - dice_coeff.mean()
